33fab6a5b964a81c3cc75a42d227460a1446bf3a
fueripe-desu <fueripedesu2020@outlook.com>
2026-06-21 23:25:53 -0300

    feat(logger): separate intentional file and console log methods

--- src/neurotic/logger.py
+++ src/neurotic/logger.py
@@ -11,6 +11,9 @@ class Logger:
         self.__file_logger = logging.getLogger(self.FILE_LOGGER_NAME)
         self.__console_logger = logging.getLogger(self.CONSOLE_LOGGER_NAME)
 
+        self.__console_initialized = False
+        self.__file_initialized = False
+
     def configure_console(
         self, level: int = logging.DEBUG, fmt: str = "[%(levelname)s]: %(message)s"
     ) -> None:
@@ -19,6 +22,7 @@ class Logger:
         consoleHandler.setFormatter(logging.Formatter(fmt))
 
         self.__console_logger.addHandler(consoleHandler)
+        self.__console_initialized = True
 
     def configure_file(
         self,
@@ -32,23 +36,62 @@ class Logger:
         fileHandler.setFormatter(logging.Formatter(fmt))
 
         self.__file_logger.addHandler(fileHandler)
+        self.__file_initialized = True
+
+    def __log_console(self, level: int, msg: object) -> None:
+        if self.__console_initialized:
+            self.__console_logger.log(level, msg)
+
+    def __log_file(self, level: int, msg: object) -> None:
+        if self.__file_initialized:
+            self.__file_logger.log(level, msg)
+
+    def console_debug(self, msg: object) -> None:
+        self.__log_console(logging.DEBUG, msg)
+
+    def console_info(self, msg: object) -> None:
+        self.__log_console(logging.INFO, msg)
+
+    def console_warning(self, msg: object) -> None:
+        self.__log_console(logging.WARNING, msg)
+
+    def console_error(self, msg: object) -> None:
+        self.__log_console(logging.ERROR, msg)
+
+    def console_critical(self, msg: object) -> None:
+        self.__log_console(logging.CRITICAL, msg)
+
+    def file_debug(self, msg: object) -> None:
+        self.__log_file(logging.DEBUG, msg)
+
+    def file_info(self, msg: object) -> None:
+        self.__log_file(logging.INFO, msg)
+
+    def file_warning(self, msg: object) -> None:
+        self.__log_file(logging.WARNING, msg)
+
+    def file_error(self, msg: object) -> None:
+        self.__log_file(logging.ERROR, msg)
+
+    def file_critical(self, msg: object) -> None:
+        self.__log_file(logging.CRITICAL, msg)
 
     def debug(self, msg: str) -> None:
-        self.__file_logger.debug(msg)
-        self.__console_logger.debug(msg)
+        self.console_debug(msg)
+        self.file_debug(msg)
 
     def info(self, msg: str) -> None:
-        self.__file_logger.info(msg)
-        self.__console_logger.info(msg)
+        self.console_info(msg)
+        self.file_info(msg)
 
     def warning(self, msg: object) -> None:
-        self.__file_logger.warning(msg)
-        self.__console_logger.warning(msg)
+        self.console_warning(msg)
+        self.file_warning(msg)
 
     def error(self, msg: object) -> None:
-        self.__file_logger.error(msg)
-        self.__console_logger.error(msg)
+        self.console_error(msg)
+        self.file_error(msg)
 
     def critical(self, msg: object) -> None:
-        self.__file_logger.critical(msg)
-        self.__console_logger.critical(msg)
+        self.console_critical(msg)
+        self.file_critical(msg)
