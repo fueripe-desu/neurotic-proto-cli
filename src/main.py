fec00045578d8e8f0bbd93d5cb9fc6f946f9a9a5
fueripe-desu <fueripedesu2020@outlook.com>
2026-06-21 22:35:59 -0300

    feat: add Logger class

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from sys import exit
 from pathlib import Path
-import sqlite3, argparse, os
+import sqlite3, argparse, os, sys, logging
 
 DATA_DIR_NAME: str = "data"
 
@@ -15,6 +15,50 @@ def create_dir(path: Path, dir_name: str):
     except Exception as e:
         print(f"An error occurred: {e}")
 
+class Logger:
+    def __init__(self, path: Path, filename: str):
+        self.__file_logger = logging.getLogger("neurotic.file_logger")
+        self.__console_logger = logging.getLogger("neurotic.console_logger")
+
+        self.__file_logger.setLevel(logging.DEBUG)
+        self.__console_logger.setLevel(logging.DEBUG)
+
+        fileHandler = logging.FileHandler(path / filename)
+        fileHandler.setFormatter(logging.Formatter(
+            fmt= "%(asctime)s [%(levelname)s]: %(message)s"
+        ))
+
+        self.__file_logger.addHandler(fileHandler)
+
+        console_formatter = logging.Formatter(
+            fmt= "[%(levelname)s]: %(message)s"
+        )
+
+        consoleHandler = logging.StreamHandler(sys.stdout)
+        consoleHandler.setFormatter(console_formatter)
+
+        self.__console_logger.addHandler(consoleHandler)
+
+    def debug(self, msg: str) -> None:
+        self.__file_logger.debug(msg)
+        self.__console_logger.debug(msg)
+
+    def info(self, msg: str) -> None:
+        self.__file_logger.info(msg)
+        self.__console_logger.info(msg)
+
+    def warning(self, msg: object) -> None:
+        self.__file_logger.warning(msg)
+        self.__console_logger.warning(msg)
+
+    def error(self, msg: object) -> None:
+        self.__file_logger.error(msg)
+        self.__console_logger.error(msg)
+
+    def critical(self, msg: object) -> None:
+        self.__file_logger.critical(msg)
+        self.__console_logger.critical(msg)
+
 class Prompt:
     prompt_prefix: str = "$:"
 
@@ -26,8 +70,10 @@ class Prompt:
         return input(f"{self.prompt_prefix} ")
 
 class Engine:
-    def __init__(self, prompt: Prompt):
+    def __init__(self, prompt: Prompt, logger: Logger):
         self.__prompt = prompt
+        self.__logger = logger
+
         self.__should_close = False
 
     def close(self) -> None:
@@ -43,7 +89,7 @@ class Engine:
                 case "quit":
                     break
                 case _:
-                    print(f"[Error]: Unknown '{prompt_value}' commmand.")
+                    self.__logger.error(f"Unknown '{prompt_value}' commmand.")
 
             if (prompt_value == "quit"):
                 break
@@ -60,14 +106,18 @@ def main():
     if not data_dir.exists():
         create_dir(cwd, DATA_DIR_NAME)
 
+    logger = Logger(data_dir, "neurotic_cli.log")
+
     if args.bebel:
         print("My beloved wife's message <3...")
         print("\"Banana\"")
         exit()
 
+    logger.debug("Starting database connection...")
     con: sqlite3.Connection = sqlite3.connect(f"{DATA_DIR_NAME}/neurotic_cli.db")
+    logger.info("Connected successfully to the database...")
 
-    engine = Engine(prompt=Prompt())
+    engine = Engine(prompt=Prompt(), logger=logger)
     engine.run()
 
 if __name__ == "__main__":
