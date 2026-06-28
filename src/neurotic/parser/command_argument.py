4c351ad0fc6fd4ef39931b2d9b61973ec16faa1d
fueripe-desu <fueripedesu2020@outlook.com>
2026-06-28 19:48:05 -0300

    fix(parser): fix CommandArgument

--- src/neurotic/parser/command_argument.py
+++ src/neurotic/parser/command_argument.py
@@ -17,15 +17,13 @@ class CommandArgument(CommandValidator):
     def __init__(self, value: str) -> None:
         super().__init__()
 
-        chars: list[str] = value.split("")
-
         self.__key: list[str] = []
         self.__value: list[str] = []
 
         dash_count: int = 0
         state: _ParserState = _ParserState.KEY
 
-        for c in chars:
+        for c in value:
             if state == _ParserState.KEY:
                 if c == "-" and len(self.__key) == 0:
                     dash_count += 1
@@ -35,7 +33,7 @@ class CommandArgument(CommandValidator):
                     if self.__key[-1] == "-":
                         return self.setError("Argument key must not end in a dash.")
 
-                elif c.isalpha() or not c.islower() or not c == "-":
+                elif not c.isalpha() and not c.islower() and not c == "-":
                     return self.setError(
                         "Argument key must only contain alphabetic characters and be separated by dashes."
                     )
@@ -54,20 +52,23 @@ class CommandArgument(CommandValidator):
                 elif c == '"' and len(self.__key) > 0:
                     state = _ParserState.END
                 else:
-                    self.__key.append(c)
+                    self.__value.append(c)
 
         if dash_count != 2:
             return self.setError(
                 "Argument key must not start with more than two dashes."
             )
 
+        self.__key_str = "".join(self.__key)
+        self.__value_str = "".join(self.__value)
+
     @override
     def __eq__(self, other: object) -> bool:
         if not isinstance(other, CommandArgument):
             return False
 
-        return self.__key == other.__key
+        return self.__key_str == other.__key_str
 
     @override
     def __hash__(self) -> int:
-        return hash(self.__key)
+        return hash(self.__key_str)
