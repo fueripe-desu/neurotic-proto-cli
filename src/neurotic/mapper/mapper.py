19896408601410fd236981345e383964c8e0a44a
fueripe-desu <fueripedesu2020@outlook.com>
2026-06-28 22:24:06 -0300

    feat(mapper): prevent registering duplicate commands

--- src/neurotic/mapper/mapper.py
+++ src/neurotic/mapper/mapper.py
@@ -2,11 +2,20 @@ from typing import Callable
 from neurotic.mapper.command_map import CommandMap
 
 
+class _DuplicateCommandError(Exception):
+    """Exception raised when the same command map has been registered twice."""
+
+    def __init__(self):
+        super().__init__("Command has been registered twice.")
+
+
 class Mapper:
     __cmd_map: dict[int, Callable[[], None]] = {}
 
-    def register_cmd(self, cmd_map: CommandMap, handler: Callable[[], None]):
-        print(cmd_map.cmd_hash)
+    def register_cmd(self, cmd_map: CommandMap, handler: Callable[[], None]) -> None:
+        if self.__cmd_map.get(cmd_map.cmd_hash) is not None:
+            raise _DuplicateCommandError()
+
         self.__cmd_map[cmd_map.cmd_hash] = handler
 
     def execute(self, cmd_hash: int) -> bool:
